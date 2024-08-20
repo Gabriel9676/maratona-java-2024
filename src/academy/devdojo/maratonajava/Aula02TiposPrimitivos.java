e8bf02235b3f65901d9cb12f00e539a1114e0863
Gabriel9676 <gabrieloliveira96@outlook.com>
2024-08-20 19:26:53 -0300

    Initial commit

---
+++ src/academy/devdojo/maratonajava/Aula02TiposPrimitivos.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+
+    }
+}
