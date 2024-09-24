829a919edc12560efde053ef25bded117c091a5b
Gabriel9676 <gabrieloliveira96@outlook.com>
2024-09-24 09:01:01 -0300

    initial commit

--- src/academy/devdojo/maratonajava/javacore/Dconsultores/test/AnimeTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Dconsultores/test/AnimeTest01.java
@@ -5,9 +5,8 @@ import academy.devdojo.maratonajava.javacore.Dconsultores.domain.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Goku", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Goku", "TV", 12, "Ação", "Production IG");
         anime.imprime();
-        anime2.imprime();
+
     }
 }
