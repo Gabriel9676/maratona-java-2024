11a5029e5579fa000077d50270cc256325438ae2
Gabriel9676 <gabrieloliveira96@outlook.com>
2024-09-30 16:39:19 -0300

    initial commit

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.domain.Jogador;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador = jogador1 = new Jogador("Pelé");
+
+
+    }
+}
