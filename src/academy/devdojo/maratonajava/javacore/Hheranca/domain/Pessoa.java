9ab89a83c5b044a3913724a9ea9f435f50851331
Gabriel9676 <gabrieloliveira96@outlook.com>
2024-10-14 20:46:49 -0300

    initial commit

--- src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Pessoa.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Pessoa.java
@@ -1,10 +1,33 @@
 package academy.devdojo.maratonajava.javacore.Hheranca.domain;
 
 public class Pessoa {
-    private String nome;
-    private String cpf;
-    private Endereco endereco;
+    protected String nome;
+    protected String cpf;
+    protected Endereco endereco;
 
+    static{
+        System.out.println("Dentro do bloco de inicialização estático de pessoa");
+    }{
+        System.out.println("Dentro do bloco de inicialização de pessoa 1");
+    }{
+        System.out.println("Dentro do bloco de inicialização de pessoa 2");
+    }
+
+    public Pessoa(String nome){
+        System.out.println("Dentro do contrutor de pessoa");
+        this.nome = nome;
+    }
+
+    public Pessoa (String nome, String cpf){
+        this(nome);
+        this.cpf = cpf;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.cpf);
+        System.out.println(this.endereco.getRua() + " " + this.endereco.getCep()) ;
+    }
     public String getNome() {
         return nome;
     }
