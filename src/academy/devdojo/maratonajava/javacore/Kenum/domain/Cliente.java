db36f4f522b5f7c6f5291e94f3accad155f074d1
Gabriel9676 <gabrieloliveira96@outlook.com>
2024-10-20 20:37:20 -0300

    initial commit

--- src/academy/devdojo/maratonajava/javacore/Kenum/domain/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/domain/Cliente.java
@@ -1,36 +1,24 @@
 package academy.devdojo.maratonajava.javacore.Kenum.domain;
 
 public class Cliente {
+
     private String noma;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
 
-
-    public Cliente(String noma, TipoCliente tipoCliente) {
+    public Cliente(String noma, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.noma = noma;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
     public String toString() {
         return "Cliente{" +
                 "noma='" + noma + '\'' +
-                ", tipoCliente=" + tipoCliente +
+                ", tipoCliente=" + tipoCliente.getNomeRelatorio() +
+                ", tipoClienteiNT=" + tipoCliente.getValor() +
+                ", tipoPagamento=" + tipoPagamento +
                 '}';
-    }
-
-    public String getNoma() {
-        return noma;
-    }
-
-    public void setNoma(String noma) {
-        this.noma = noma;
-    }
-
-    public TipoCliente getTipoCliente() {
-        return tipoCliente;
-    }
-
-    public void setTipoCliente(TipoCliente tipoCliente) {
-        this.tipoCliente = tipoCliente;
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClientTest01.java
@@ -2,17 +2,16 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.domain.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.domain.TipoPagamento;
 
 public class ClientTest01 {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
-
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
