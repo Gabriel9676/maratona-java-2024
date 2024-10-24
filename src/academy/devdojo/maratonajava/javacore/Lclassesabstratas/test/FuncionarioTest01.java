f0d24d3b125ccec18ef283a6807175be70fd3a24
Gabriel9676 <gabrieloliveira96@outlook.com>
2024-10-23 22:40:39 -0300

    initial commit

--- src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -11,5 +11,8 @@ public class FuncionarioTest01 {
         Desenvolvedor desenvolvedor = new Desenvolvedor("Touya", 120000);
         System.out.println(gerente);
         System.out.println(desenvolvedor);
+        gerente.imprime();
+        desenvolvedor.imprime();
+        gerente.imprime();
     }
 }
